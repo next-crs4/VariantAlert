1f0c04452d9e88ece4ee61dd8dbef2df7d16dabf
ratzeni <rossano.atzeni@crs4.it>
2020-07-09 18:19:34 +0200

    Send a cumulative email for all the updates of the day. #7

--- src/variant_alert/cron.py
+++ src/variant_alert/cron.py
@@ -13,29 +13,31 @@ from django.utils.timezone import now
 
 
 class QueryCronJob(CronJobBase):
-
     RUN_AT_TIMES = ['00:00']
 
     schedule = Schedule(run_at_times=RUN_AT_TIMES)
 
-    code = 'variant_alert.my_cron_job'    # a unique code
+    code = 'variant_alert.my_cron_job'  # a unique code
 
-    def __alert(self, q):
-        url = os.path.join(settings.HOST, 'variants', 'query', str(q.id))
+    def __alert(self, u, queries):
+        self.logger.info('Preparing mail for user: {} - {}'.format(u.get('username'), u.get('email')))
+        msgs = "\n"
+        base_url = os.path.join('http://'+settings.HOST, 'variants', 'query')
+        for q in queries:
+            url = os.path.join(base_url, str(q.id))
+            msgs = msgs + "\t" + " q.label " + " - " + q.query + " (Go to {} for details)\n".format(url)
 
-        for u in User.objects.all():
-            if u.id == q.user_id:
-                self.logger.info('Preparing mail for user: {} - {}'.format(u.username, u.email))
-                try:
-                    email = EmailMessage(
-                        "VariantAlert updates - {}".format(now()),
-                        "Dear {}\n, Changes in annotations were detected for the following variant(s): {}: {}.\n"
-                        "Go to {} for details. \nVariantAlert Team ".format(u.username, q.label, q.query, url),
-                        to=[u.email])
-                    email.send()
-                    self.logger.info('Mail sent to user: {} - {}'.format(u.username, u.email))
-                except Exception as e:
-                    self.logger.error(e)
+        subject = "VariantAlert updates - {}".format(now())
+        body = "Dear {},\n".format(u.get('username')) \
+               + "Changes in annotations were detected for the following variant(s):\n " \
+               + msgs + "\nBest Regards\nThe VariantAlert Team ({})".format(settings.EMAIL_HOST_USER)
+        try:
+            email = EmailMessage(from_email=settings.EMAIL_HOST_USER,
+                                 subject=subject, body=body, to=[u.get('email')])
+            email.send()
+            self.logger.info('Mail sent to user: {} - {}'.format(u.get('username'), u.get('email')))
+        except Exception as e:
+            self.logger.error(e)
 
     def a_logger(self, name, level="INFO", filename=None, mode="a"):
         log_format = '%(asctime)s|%(levelname)-8s|%(name)s |%(message)s'
@@ -57,24 +59,28 @@ class QueryCronJob(CronJobBase):
         return logger
 
     def do(self):
+        v = Variants()
+        alerts = list()
         self.logger = self.a_logger(self.__class__.__name__)
         self.logger.info('=== START ===')
-        self.logger.info('Collecting Queries...')
-        queries = QueryModel.objects.all()
-        v = Variants()
-        for q in queries:
-            self.logger.info('Checking for changes in the query {} - user {}'.format(q.label, q.user_id))
-            q.previous = q.result
-            q.result = v.get_variant(q.query, q.assembly)
-            q.difference = compare(q.previous, q.result)
-            q.date = q.date
-            if q.difference:
-                self.logger.info('ALERT - Found change for query {} - user {}'.format(q.label, q.user_id))
-                q.update = now()
-                q.save()
-                self.__alert(q)
-            else:
-                self.logger.info('Nothing is changed for query {} - user {}'.format(q.label, q.user_id))
-             
+        self.logger.info('Collecting Users...')
+        for u in User.objects.values():
+            self.logger.info("Collecting Queries for {} - {}".format(u.get('username'), u.get('email')))
+            queries = QueryModel.objects.filter(user_id=u.get('id'))
+            for q in queries:
+                self.logger.info('Checking for changes in the query {} - {}'.format(q.label, q.query))
+                q.previous = q.result
+                q.result = v.get_variant(q.query, q.assembly, q.fields)
+                q.difference = compare(q.previous, q.result)
+                q.date = q.date
+                if q.difference:
+                    self.logger.info('ALERT - Found change for query {} - {}'.format(q.label, q.query))
+                    q.update = now()
+                    #q.save()
+                    alerts.append(q)
+                else:
+                    self.logger.info('Nothing is changed for query {} - {}'.format(q.label, q.query))
+            if alerts:
+                self.__alert(u, alerts)
 
-
+        self.logger.info('=== END ===')
