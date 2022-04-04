b62993382447d5b0134765b95992689a3efffc2c
ratzeni <rossano.atzeni@crs4.it>
2022-04-04 09:10:53 +0200

    Models

--- src/models.py
+++ src/models.py
@@ -1,9 +1,153 @@
-from django.db import models 
-from django.core.validators import MaxValueValidator, MinValueValidator
+# This is an auto-generated Django model module.
+# You'll have to do the following manually to clean this up:
+#   * Rearrange models' order
+#   * Make sure each model has one field with primary_key=True
+#   * Make sure each ForeignKey has `on_delete` set to the desired behavior.
+#   * Remove `managed = False` lines if you wish to allow Django to create, modify, and delete the table
+# Feel free to rename the models, but don't rename db_table values or field names.
+from django.db import models
 
-class Query(models.Model):
-    User = settings.AUTH_USER_MODEL
-    user = models.ForeignKey(User, null=True)
-    name = models.CharField(max_length=500)
-    description = models.TextField()
-    chr = IntegerRangeField(min_value=1, max_value=50)
+
+class AuthGroup(models.Model):
+    name = models.CharField(unique=True, max_length=150)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group'
+
+
+class AuthGroupPermissions(models.Model):
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+    permission = models.ForeignKey('AuthPermission', models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_group_permissions'
+        unique_together = (('group', 'permission'),)
+
+
+class AuthPermission(models.Model):
+    name = models.CharField(max_length=255)
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING)
+    codename = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_permission'
+        unique_together = (('content_type', 'codename'),)
+
+
+class AuthUser(models.Model):
+    password = models.CharField(max_length=128)
+    last_login = models.DateTimeField(blank=True, null=True)
+    is_superuser = models.BooleanField()
+    username = models.CharField(unique=True, max_length=150)
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=150)
+    email = models.CharField(max_length=254)
+    is_staff = models.BooleanField()
+    is_active = models.BooleanField()
+    date_joined = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user'
+
+
+class AuthUserGroups(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    group = models.ForeignKey(AuthGroup, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_groups'
+        unique_together = (('user', 'group'),)
+
+
+class AuthUserUserPermissions(models.Model):
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    permission = models.ForeignKey(AuthPermission, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'auth_user_user_permissions'
+        unique_together = (('user', 'permission'),)
+
+
+class DjangoAdminLog(models.Model):
+    action_time = models.DateTimeField()
+    object_id = models.TextField(blank=True, null=True)
+    object_repr = models.CharField(max_length=200)
+    action_flag = models.SmallIntegerField()
+    change_message = models.TextField()
+    content_type = models.ForeignKey('DjangoContentType', models.DO_NOTHING, blank=True, null=True)
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+
+    class Meta:
+        managed = False
+        db_table = 'django_admin_log'
+
+
+class DjangoContentType(models.Model):
+    app_label = models.CharField(max_length=100)
+    model = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'django_content_type'
+        unique_together = (('app_label', 'model'),)
+
+
+class DjangoCronCronjoblog(models.Model):
+    code = models.CharField(max_length=64)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    is_success = models.BooleanField()
+    message = models.TextField()
+    ran_at_time = models.TimeField(blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'django_cron_cronjoblog'
+
+
+class DjangoMigrations(models.Model):
+    app = models.CharField(max_length=255)
+    name = models.CharField(max_length=255)
+    applied = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_migrations'
+
+
+class DjangoSession(models.Model):
+    session_key = models.CharField(primary_key=True, max_length=40)
+    session_data = models.TextField()
+    expire_date = models.DateTimeField()
+
+    class Meta:
+        managed = False
+        db_table = 'django_session'
+
+
+class VariantsQuerymodel(models.Model):
+    label = models.CharField(max_length=50)
+    chromosome = models.CharField(max_length=5)
+    position = models.IntegerField()
+    variant_ref = models.CharField(max_length=1)
+    variant_alt = models.CharField(max_length=1)
+    assembly = models.CharField(max_length=5)
+    query = models.CharField(max_length=25)
+    result = models.TextField()  # This field type is a guess.
+    previous = models.TextField()  # This field type is a guess.
+    difference = models.TextField()  # This field type is a guess.
+    fields = models.TextField()  # This field type is a guess.
+    date = models.DateTimeField()
+    update = models.DateTimeField()
+    user = models.ForeignKey(AuthUser, models.DO_NOTHING)
+    alert = models.BooleanField()
+
+    class Meta:
+        managed = True
+        db_table = 'variants_querymodel'
